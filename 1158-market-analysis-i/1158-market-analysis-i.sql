394943f6fe42de182e5d86220db477a51bd931a4
유경헌 <92325898+yootal@users.noreply.github.com>
2025-10-15 18:17:18 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,9 +1,8 @@
-select u.user_id as buyer_id, u.join_date, ifnull(o.c,0) as orders_in_2019
-from Users u
-left join (
-    select buyer_id, count(*) as c
-    from Orders
-    where year(order_date) = 2019
-    group by buyer_id
-) o on u.user_id = o.buyer_id
-
+SELECT 
+  u.user_id AS buyer_id,
+  u.join_date,
+  SUM(o.order_date >= '2019-01-01' AND o.order_date < '2020-01-01') AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o
+  ON o.buyer_id = u.user_id
+GROUP BY u.user_id, u.join_date;
