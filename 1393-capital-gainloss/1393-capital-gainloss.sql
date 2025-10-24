bbd8e431c01469c8659a02289e2ebf8008d10023
유경헌 <92325898+yootal@users.noreply.github.com>
2025-10-24 15:49:27 +0900

    Time: 939 ms (5.4%), Space: 0B (100%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,6 +1,5 @@
 select 
     stock_name, 
-    SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) 
-    - SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) as capital_gain_loss
+    SUM(CASE WHEN operation = 'Sell' THEN price ELSE -price END) as capital_gain_loss
 from Stocks
 group by stock_name
