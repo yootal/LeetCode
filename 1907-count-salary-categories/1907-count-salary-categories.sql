d017a30a6d07ac671f6b1fb5cf844513ae4b19fd
유경헌 <92325898+yootal@users.noreply.github.com>
2025-10-25 18:57:09 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,17 @@
+select 
+    "Low Salary" as category,
+    count(*) as accounts_count
+from Accounts
+where income <= 20000
+union
+select 
+    "Average Salary" as category,
+    count(*) as accounts_count
+from Accounts
+where income between 20000 and 50000
+union
+select 
+    "High Salary" as category,
+    count(*) as accounts_count
+from Accounts
+where income > 50000
