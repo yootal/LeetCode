8e6ecb3ef877883a68b166af028eb70707f95693
유경헌 <92325898+yootal@users.noreply.github.com>
2025-10-30 17:10:40 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -0,0 +1,5 @@
+select transaction_date, 
+    sum(case when amount % 2 = 1 then amount else 0 end) as odd_sum, 
+    sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
+from transactions
+group by transaction_date
