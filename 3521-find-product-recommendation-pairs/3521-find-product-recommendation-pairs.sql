ebebe1ec3cd0a30c2a09e84dae9333195ec04170
유경헌 <92325898+yootal@users.noreply.github.com>
2025-11-04 23:54:43 +0900

    Time: 727 ms (16.64%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -3,7 +3,7 @@ SELECT
     p2.product_id AS product2_id,
     i1.category  AS product1_category,
     i2.category  AS product2_category,
-    COUNT(DISTINCT p1.user_id) AS customer_count
+    COUNT(*) AS customer_count
 FROM ProductPurchases p1
 JOIN ProductPurchases p2
     ON p1.user_id = p2.user_id
@@ -11,7 +11,7 @@ JOIN ProductPurchases p2
 JOIN ProductInfo i1 ON p1.product_id = i1.product_id
 JOIN ProductInfo i2 ON p2.product_id = i2.product_id
 GROUP BY p1.product_id, p2.product_id
-HAVING COUNT(DISTINCT p1.user_id) >= 3
+HAVING COUNT(*) >= 3
 ORDER BY
     customer_count DESC,
     product1_id ASC,
