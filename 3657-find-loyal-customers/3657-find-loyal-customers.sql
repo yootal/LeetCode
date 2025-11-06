eba42808d8c27d1e88c2f41f010bc1f7280e03a0
유경헌 <92325898+yootal@users.noreply.github.com>
2025-11-06 15:48:32 +0900

    Time: 378 ms (9.61%), Space: 0B (100%) - LeetHub

---
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -0,0 +1,8 @@
+SELECT customer_id
+FROM customer_transactions
+GROUP BY customer_id
+HAVING
+  SUM(CASE WHEN transaction_type = 'purchase' THEN 1 ELSE 0 END) >= 3
+  AND DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30
+  AND (SUM(CASE WHEN transaction_type = 'refund' THEN 1 ELSE 0 END) * 1.0 / COUNT(*)) < 0.20
+ORDER BY customer_id;
